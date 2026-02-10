99870bd10a6efb74c24f74ba4ce9cb9fd10578d4
Santiago Granados Pascagaza <sgranadosp@unbosque.edu.co>
2026-02-10 13:27:30 -0500

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://sgranadosp.github.io',
   integrations: [mdx(), sitemap()],
 
   vite: {
