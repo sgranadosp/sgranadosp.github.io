cb113d9d3494fb3c49f7f51820ac3aab1fa4095b
Santiago Granados Pascagaza <sgranadosp@unbosque.edu.co>
2026-02-15 21:15:03 -0500

    borrar iconos de los proyectos

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,39 +1,30 @@
-import PhoenixIcon from '../assets/logos/Phoenix-logo.svg?raw';
-import N8nIcon from '../assets/logos/N8n-logo.svg?raw';
-import SupabaseIcon from '../assets/logos/Supabase-logo.svg?raw';
-import PosthogIcon from '../assets/logos/posthog-logo.svg?raw';
-
 export const projects = [
 	{
 		title: "Front-End Aplicación de Conversión de Formato de Imágenes",
 		techStack: "React • JavaScript • TypeScript • CSS • HTML",
 		description: "Interfaz visual hecha a través de React, la cual busca brindar una experiencia agradable visualmente y sobre todo un uso muy intuitivo al usuario final de esta web de conversión de formato de imágenes.",
 		ctaText: "Ir al repositorio →",
-		ctaLink: "#",
-		icon: N8nIcon
+		ctaLink: "#"
 	},
 	{
 		title: "Back-End Aplicativo de gestión de citas médicas para el centro de salud Los Cobos",
 		techStack: "Java • SpringBoot",
 		description: "Aplicativo simple para la gestión de citas médicas, turnos y otras tareas de suma importancia en la clínica Los Cobos de la Universidad El Bosque.",
 		ctaText: "Ir al repositorio →",
-		ctaLink: "#",
-		icon: SupabaseIcon
+		ctaLink: "#"
 	},
 	{
 		title: "Back-End y Front-end Aplicativo de traducción de texto",
 		techStack: "Java",
 		description: "Aplicativo simple para la traducción de texto a diversos idiomas, intuitivo y fácil de utilizar.",
 		ctaText: "Ir al repositorio →",
-		ctaLink: "#",
-		icon: PhoenixIcon
+		ctaLink: "#"
 	},
 	{
 		title: "Back-End y Front-End para la gestión de inventario de una tienda tecnológica",
 		techStack: "Java",
 		description: "Aplicativo simple con herramientas para agregar, eliminar, consultar y actualizar métricas de inventario para una tienta tecnológica ubicada en Bogotá, Usaquén.",
 		ctaText: "Ir al repositorio →",
-		ctaLink: "#",
-		icon: PosthogIcon
+		ctaLink: "#"
 	},
 ];
